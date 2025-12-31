cf92274a30670e9dd0e17576887bde7aab5593af
Shanabu-6 <60versets_mentholes@icloud.com>
2025-12-31 13:20:43 +0100

    maj

--- programmes/script_french.sh
+++ programmes/script_french.sh
@@ -6,71 +6,67 @@ DIR_ASPI="./aspirations"
 DIR_CONTXT="./contextes"
 DIR_DUMP="./dumps-text"
 DIR_CONCORD="./concordances"
-
-
+DIR_HTML_OUT="./tableaux/état_français.html"
 
 numero_ligne=0
 
 # Vérification arguments 
-if [ $# -ne 1 ]
-then
+if [ $# -ne 1 ] ; then
 	echo "Usage : ce programme nécessite 1 argument : urls.txt"
 	exit 1
 fi
 
-if [ ! -f "$URL_FILE" ]
-then
+if [ ! -f "$URL_FILE" ] ; then
     echo "Erreur : le fichier '$URL_FILE' n'a pas été trouvé"
     exit 1
 fi
 
-{
 
-echo "<html>
-	<head>
-		<meta charset=\"UTF-8\">
-	</head>
+# Création du tableau HTML
 
-	<body>
-		<table>
-			<tr>
-				<th>numero_ligne</th>
-				<th>URL</th>
-				<th>code</th>
-				<th>encodage</th>
-				<th>nb_mots</th>
-			</tr>"
-		
+TAB_HTML=$(
+	echo "<!DOCTYPE html>"
+	echo "<html>"
+	echo "<head>"
+	echo "<title>Projet de groupe PPE-25/26, français</title>"
+	echo "<link rel=\"stylesheet\" href=\"https://cdn.jsdelivr.net/npm/bulma@1.0.2/css/bulma.min.css\">"
+	echo "</head>"
+	
+	echo "<style>"
+    echo ".content{overflow:scroll}"
+	echo "</style>"
+	
+	echo "<body>"
+	echo "<h2 class=\"title\" >Tableau <code>français</code></h2>"
+	echo "<table class=\"table\" >"
+	echo "<tr>"
+
+	
+
+	# Colones du tableau
+
+	echo "<td>" "num Ligne" "</td>"
+	echo "<td>" "URL" "</td>"
+	echo "<td>" "Code HTTP" "</td>"
+	echo "<td>" "Encodage" "</td>"
+	echo "<td>" "Nombre d'occurences" "</td>"
+	echo "<td>" "Page HTML brute" "</td>"
+	echo "<td>" "Dump textuel" "</td>"
+	echo "<td>" "Concordancier HTML" "</td>"
+
+	echo "</tr>"
+	echo "</table>"
+	echo "</body>"
+	echo "</html>"
+
+)
+
+echo "$TAB_HTML" > "$DIR_HTML_OUT"
 
 
 
 
-while read -r URL 
-do
 
-	numero_ligne=$((numero_ligne + 1))
-    
-    onlyRequest=$(curl -sS "$URL")
-
-    #code=$(curl -s -o /dev/null -w "%{http_code}" "$URL")
-    encodage=$(echo "$onlyRequest" | grep -i charset | cut -d= -f2)
-    nb_mots=$(echo "$onlyRequest" | wc -w)
-
-    echo "<tr>
-        	<td>$numero_ligne</td>
-        	<td>$URL</td>
-        	<td>$code</td>
-        	<td>$encodage</td>
-        	<td>$nb_mots</td>
-   		 </tr>"
-
-done < "$URL_FIL"
-
-echo "</table>	
-	</body>
-</html>"
-
-} > output2.html
 
 
 
