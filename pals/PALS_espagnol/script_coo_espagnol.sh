475851fb36a0e14ba53e2df13d1adc746d3e41de
Farah <akirdelouafifarah@gmail.com>
2026-01-03 23:56:36 +0100

    mkdir PALS_ espagnol

---
+++ pals/PALS_espagnol/script_coo_espagnol.sh
@@ -0,0 +1,11 @@
+script_coo_espagnol
+
+#!/bin/bash
+
+# ------------------------
+# Script : cooccurrents pour "estado"
+# ------------------------
+
+CORPUS_DIR="../../dumps-text/espagnol"
+OUTPUT_HTML="../../tableaux/estado_cooc.html"
+TARGET="estado.*"
