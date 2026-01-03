ad62df7736cda82c27a770f09affc6fde4441da3
Farah <akirdelouafifarah@gmail.com>
2026-01-03 16:16:36 +0100

    maj espagnol

--- programmes/espagnol/script_espagnol.sh
+++ programmes/espagnol/script_espagnol.sh
@@ -12,9 +12,7 @@ DIR_DUMP="./dumps-text/espagnol/"
 DIR_CONCORD="./concordances/espagnol"
 DIR_HTML_OUT="./tableaux/estado_espagnol.html"
 
-
 USER_AGENT="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
-
 
 # Vérification arguments 
 if [ $# -ne 1 ] ; then
@@ -22,12 +20,12 @@ if [ $# -ne 1 ] ; then
 	exit 1
 fi
 
-if [ ! -f "$URL_FILE" ] ; then
+if [ ! -f "$URL_FICHIER" ] ; then
     echo "Erreur : le fichier '$URL_FILE' n'a pas été trouvé"
     exit 1
 fi
 
-# Création du tableau HTML
+# Création du tableau HTML espagnol
 
 TAB_HTML=$(
 	echo "<!DOCTYPE html>"
@@ -66,27 +64,45 @@ TAB_HTML=$(
 # compteur qui va numéroter les fichiers (ex : dump_1) 
 ID=1
 
-
 # ouverture de la boucle qui va lire mon fichier espagnol ligne par ligne
 while read -r URL ; do
 
-# On crée on fichier temporaire pour stocker la page HTML brute (pour ne pas ralentir ou faire planter le script)
-raw_file="/tmp/raw_$ID.html"
+# On crée on fichier temporaire (tmp) pour stocker la page HTML brute (pour ne pas ralentir ou faire planter le script)
+# facile à nettoyer après
+TEMP_HTML="/tmp/raw_$ID.html"
 
-# On vérifie le code HTTP
-if [[ "$http_code" != "200" ]]; then
-    echo "Erreur : $URL non traité (HTTP $http_code)"
+# Requête HTTP pour récupérer la page (boucle if)
+    # -s : silencieux (pas de barre de progression)
+    # -L : suivre les redirections
+    # -A : définit l'user-agent (simule un navigateur)
+    # -o : écrit le contenu dans TEMP_HTML
+    # -w "%{HTTP_CODE}" : récupère le code HTTP en sortie
+
+HTTP_CODE=$(curl -s -L -A "$USER_AGENT" -o "$TEMP_HTML" -w "%{http_code}" "$URL")
+
+# Vérification du code HTTP
+
+if [[ "$HTTP_CODE" != "200" ]]; then
+    echo "Erreur : $URL non traité (HTTP $HTTP_CODE)"
     continue   # passe à l'URL suivante
 fi
 
-
 # On copie la page HTML brute dans le dossier "aspirations"
-filename_aspiration="$DIR_ASPI/es-$ID.html"
-cp "$raw_file" "$filename_aspiration"
 
+FILE_ASPI="$DIR_ASPI/es-$ID.html"
+cp "$TEMP_HTML" "$FILE_ASPI"
 
+# On détecte l'encodage : 
 
+ENCODING=$(file --mime-encoding --brief "$FILE_ASPI")
+    echo "L'encodage est le suivant : $ENCODING"
 
+# On va maintenant convertir le contenu HTML en texte brut (sans les balises)
+# Avec la commande sed -i on va supprimer : 
+	#[^...] : tout ce qui n’est pas dans la liste
+	#[:alnum:] : lettres et chiffres classiques
+	#[:space:] : espaces, tabulations, retour à la ligne
+	# ñÑáéíóúüÁÉÍÓÚÜ : caractères spécials espagnols que l'on souhaite garder
 
 
 
