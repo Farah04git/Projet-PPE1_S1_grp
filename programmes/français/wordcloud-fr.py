24a006eb75ac4f38264ed208a802011e8824766c
Shanabu-6 <60versets_mentholes@icloud.com>
2026-01-04 15:35:50 +0100

    fr : wordcloud

--- programmes/français/wordcloud-fr.py
+++ programmes/français/wordcloud-fr.py
@@ -1,15 +1,41 @@
 import os, chardet, re
+import matplotlib.pyplot as plt
 
 dumps_folder = "dumps-text/français/"
+output = "output-wordcloud.txt"
 
+
+def read_convert(path):
+    with open(path, "rb") as f:
+        raw = f.read()
+
+    result_chardet = chardet.detect(raw)
+
+    try:
+        text = raw.decode(enc)
+    except Exception:
+        text = raw.decode("latin-1", errors='replace')
+
+    return text
 
 def pwd():
     os.getcwd()
 
 def recupDump():
     all_dumps = ""
+    
     for files in os.listdir(dumps_folder):
-        print(files)
+        path = os.path.join(dumps_folder, files)
+        
+        txt = read_convert(path)
+        all_dumps += txt + "\n"
+
+    with open(output, 'w', encoding='utf-8') as out:
+        out.write(all_dumps)
+            
+ 
+            
+
 
 pwd()
 
